ca5c012c97ca9408359b4fb1e0bd99cdfa4f3b49
lichaoqun <121708983@qq.com>
2019-03-31 13:33:02 +0800

    Initial Commit

---
+++ QGProject/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  QGProject
+//
+//  Created by QG on 2019/3/31.
+//  Copyright © 2019年 xinxianzhizao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ QGProject/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  QGProject
+//
+//  Created by QG on 2019/3/31.
+//  Copyright © 2019年 xinxianzhizao. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+@end
